18d27d592f3375cbf9f46687ea88e1295eac15be
tower <zhangst1019@163.com>
2017-04-17 14:38:39 +0800

    changes log

---
+++ app/src/main/java/com/tower/simpleread/MainActivity.java
@@ -0,0 +1,19 @@
+package com.tower.simpleread;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        init();
+    }
+
+    private void init() {
+        //这是一个空的方法，其实什么有没有干
+    }
+}
