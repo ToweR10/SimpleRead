ff035bbf834e6e9ee593eefeabdb615fa209ed73
tower <zhangst1019@163.com>
2017-04-17 15:43:01 +0800

    Tower

--- app/src/main/java/com/tower/simpleread/MainActivity.java
+++ app/src/main/java/com/tower/simpleread/MainActivity.java
@@ -2,41 +2,12 @@ package com.tower.simpleread;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
-    private TextView tvname;
-    private Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        init();
-        setListener();
-    }
-
-    private void setListener() {
-        tvname.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                
-            }
-        });
-        btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-            }
-        });
-    }
-
-    private void init() {
-        tvname = (TextView) findViewById(R.id.tv_name);
-        btn = (Button) findViewById(R.id.btn);
     }
 }
